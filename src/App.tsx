282d69e02f88a5d5308195a8bfbfbea903b59f5a
hit0tsu__ <49010409+hit0tsuu@users.noreply.github.com>
2022-01-20 00:23:29 +0300

    переписал каркас на ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React, {FC} from 'react';
-import { Button } from 'antd';
+
 import './App.css';
 import "antd/dist/antd.css";
 
 const App:FC = () => {
   return (
     <div className="App">
-      <Button type='primary'>AAAAAAA</Button>
+
     </div>
   );
 }
