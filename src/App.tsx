ac5430a5969035c4d523eb50406be31c106f3d01
hit0tsu__ <49010409+hit0tsuu@users.noreply.github.com>
2022-01-19 15:42:17 +0300

    установил все зависимости

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {FC} from 'react';
+import { Button } from 'antd';
 import './App.css';
+import "antd/dist/antd.css";
 
-function App() {
+const App:FC = () => {
   return (
     <div className="App">
-
+      <Button type='primary'>AAAAAAA</Button>
     </div>
   );
 }
