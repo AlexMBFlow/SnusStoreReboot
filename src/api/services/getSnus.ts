4203cc2b9707ac30c656da0000c948ae4fde8e04
hit0tsu__ <Maltzev.alexandr@gmail.com>
2022-09-17 01:33:26 +0300

    add types x2

--- src/api/services/getSnus.ts
+++ src/api/services/getSnus.ts
@@ -1,6 +1,7 @@
+import { AppDispatch } from './../../redux/store';
 import { GET_SNUS_FROM_FETCH } from "../../redux/utils/actionTypes";
 
-export const getSnus = async () => {
+export const getSnus = async (): Promise<(dispatch: AppDispatch) => void> => {
     return dispatch => {
         fetch("http://localhost:5000/api/snus").then(res => res.json()).then(json => {
             const parse = JSON.parse(json)
